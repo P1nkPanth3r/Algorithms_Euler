22c52b852541c61c6c7eb49f236523b9ffe6736a
Tyler Thomas <Chessmasterty@gmail.com>
2018-07-10 22:31:17 -0400

    Update Problem15.py

--- Problem15.py
+++ Problem15.py
@@ -7,7 +7,7 @@
 # binomial distribution and simply solve the resulting combinations. Thus, we can start by creating a vector with 40 indices,
 # starting with 20 r's and then 20 d's (obviously there is only one way to do this). Then, we can imagine taking a single r
 # and placing it on the "right-hand" side (somewhere in the last 20 indices). There are 20-choose-1 = 20 ways to do this
-# (one way for each index). However, this also means we are de-facto placing one d on the "left-hand" side (somewhere in
+# (one way for each index). However, this also means we are de facto placing one d on the "left-hand" side (somewhere in
 # the first 20 indices) which can also happen 20-choose-1 = 20 ways, so in total we have (20-choose-1)**2 = 400
 # possibilities. In general, since this sequence of 20-choose-i for 0 <= i <= 20 is binomial it follows pascal's triangle,
 # so the number of routes is simply the sum of the squared values at the 20th row of the triangle.
